915a60e27d20ecfe12f2db0f94680e4de7f2ae82
Zernova_Marina <mari4793@yandex.ru>
2020-10-21 02:02:21 +0300

    M03. Strings Overview. Formatting, Parsing, Comparing

---
+++ M03_Strings/ConsoleApp/ParseString.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class ParseString
+    {
+        public static string[] FindNumbers (string text)
+        {
+            System.Diagnostics.Debug.Assert (!string.IsNullOrEmpty (text), "The string is empty");
+
+            Regex rgx = new Regex(@"(\+)?(\d{1,3})[- _]*\(?[- _]*(\d{2,3}[- _]*\)?([- _]*\d){7}|\d\d[- _]*\d\d[- _]*\)?([- _]*\d){6})");
+            MatchCollection aMatches = rgx.Matches (text);
+
+            var result = new string [aMatches.Count];
+            for (int i = 0; i < aMatches.Count; i++)
+            {
+                result[i] = aMatches[i].Value;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ M03_Strings/ConsoleApp/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string average = "In the Choose a strong name key file box, choose Browse, and then navigate to the key file.";
+            Console.WriteLine ("Test string : " + average);
+            Console.WriteLine ("Average word length in an input string : " + StringParameters.GetAverageWordLength (average));
+            Console.WriteLine();
+
+            string first = "omg i love shrek";
+            string second = "o kek";
+            Console.WriteLine ("First string  : " + first );
+            Console.WriteLine ("Second string : " + second);
+            Console.WriteLine ("Double in the first string all characters belonging the second string : " + ModifyString.DoubleStringCharacters (first, second));
+            Console.WriteLine();
+
+            string num1 = "4455654687987909809545646464654518643";
+            string num2 = "515648643166431545641351315";
+            Console.WriteLine ("First number  : " + num1);
+            Console.WriteLine ("Second number : " + num2);
+            Console.WriteLine ("Sum of two big numbers : " + BigNumbers.Add (num1, num2));
+            Console.WriteLine();
+
+            string reverse = "The greatest victory is that which requires no battle";
+            Console.WriteLine ("Test string : " + reverse);
+            Console.WriteLine ("Reverse all of the words within the string : " + ModifyString.ReverseWords (reverse));
+            Console.WriteLine();
+
+            try
+            {
+                string text = File.ReadAllText("../../../Text.txt");
+                string[] numbers = ParseString.FindNumbers(text);
+                StreamWriter sw = new StreamWriter("../../../Numbers.txt", false, Encoding.Default);
+                Console.WriteLine("Numbers : ");
+                foreach (var number in numbers)
+                {
+                    Console.WriteLine(number);
+                    sw.WriteLine(number);
+                }
+                sw.Close();
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine ("Directory not found " + ex.ToString());
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("File not found " + ex.ToString());
+            }
+        }
+    }
+}
